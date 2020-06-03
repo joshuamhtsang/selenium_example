77b796dc0dc29463ee53c23d40a24f71609c9370
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-06-03 22:54:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # selenium_example
 My Selenium playground.
+
+# Setup Instructions
+
+1. Install selenium with pip3.
+    $ pip3 install selenium
+
+2. Add chromedriver to your path, you should download the
+correct version from here:
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+You can check the version of your Chrome using 'About Google Chrome'.
+
+    $ unzip chromedriver_linux64.zip
+    $ sudo mv chromedriver /usr/bin/chromedriver
+    $ sudo chown root:root /usr/bin/chromedriver
+    $ sudo chmod +x /usr/bin/chromedriver
+
+3. You can now run the example:
+
+    $ python3 try_selenium.py
+
+
+
+
+
+
---
+++ selenium_tiktok.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.tiktok.com/trending")
+print(driver.page_source)
+# Get scroll height.
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    # Scroll down to the bottom.
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load the page.
+    time.sleep(20)
+    # Calculate new scroll height and compare with last scroll height.
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+driver.close()
