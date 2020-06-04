c37bb6b1d6883b09b328a40e7ee61dc59032df8f
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-06-04 12:20:16 +0100

    change source output order

--- selenium_tiktok.py
+++ selenium_tiktok.py
@@ -11,13 +11,14 @@ while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     # Wait to load the page.
     time.sleep(20)
+    # Output html source to file.
+    with open("page_source.html", 'w') as filehandle:
+        filehandle.write(driver.page_source)
+    
     # Calculate new scroll height and compare with last scroll height.
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
     last_height = new_height
-
-    with open("page_source.html", 'w') as filehandle:
-        filehandle.write(driver.page_source)
 
 driver.close()
