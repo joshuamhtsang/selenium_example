a3f976a130ac6fc1ddd16057c5dd55d957b537d3
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-06-04 15:09:10 +0100

    count unique videos

---
+++ analyze_html.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+with open("page_source_copy.html", "r") as f:
+    content = f.read()
+
+    soup = BeautifulSoup(content, "lxml")
+
+    videos = soup.find_all("a", class_="jsx-2214895887 jsx-3216645737 item-video-card-wrapper")
+    print(len(videos))
+
+    unique_videos = set()
+
+    for video in videos:
+        print(video['href'])
+        video_url = video['href']
+
+        unique_videos.add(video_url)
+
+    print(len(unique_videos))
