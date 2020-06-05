7cb1999ec6c3327e509276c64a80a759139ab8e1
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-06-05 12:04:38 +0100

    added argparser

--- analyze_html.py
+++ analyze_html.py
@@ -1,19 +1,28 @@
 from bs4 import BeautifulSoup
+import argparse
 
-with open("page_source_12.html", "r") as f:
-    content = f.read()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="HTML file to analyse.")
+    args = parser.parse_args()
 
-    soup = BeautifulSoup(content, "lxml")
+    with open(args.input, "r") as f:
+        content = f.read()
 
-    videos = soup.find_all("a", class_="jsx-2214895887 jsx-3216645737 item-video-card-wrapper")
-    print(len(videos))
+        soup = BeautifulSoup(content, "lxml")
 
-    unique_videos = set()
+        videos = soup.find_all("a", class_="jsx-2214895887 jsx-3216645737 item-video-card-wrapper")
+        print(len(videos))
 
-    for video in videos:
-        print(video['href'])
-        video_url = video['href']
+        all_videos = list()
+        unique_videos = set()
 
-        unique_videos.add(video_url)
+        for video in videos:
+            print(video['href'])
+            video_url = video['href']
 
-    print(len(unique_videos))
+            all_videos.append(video_url)
+            unique_videos.add(video_url)
+
+        print("Number of videos = ", len(all_videos))
+        print("Number of unique videos = ", len(unique_videos))
